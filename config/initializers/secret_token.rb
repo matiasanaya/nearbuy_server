bc52f7b2750128ce571df441e2be89860bb230d8
Matias Anaya <matiasanaya@gmail.com>
2013-06-15 12:43:01 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NearbuyServer::Application.config.secret_token = '84be70b63b872e2d0c7301ff9d03a5b21ba864b35a5c3536ee7a7090717f6d3d04283df95a70575eb024b807514405667cdd3c569100817e1ea5a95615763b37'
